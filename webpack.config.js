821e4c5d782e412fd733b9c029bff489a77c7146
yws3806 <yws3806@gmail.com>
2024-08-04 16:53:30 +0900

    webpack.config.js entry 경로 이름 변경

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require("path");
 
 module.exports = {
   mode: "development",
-  entry: "./src/index.js",
+  entry: "./src/client.js",
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
